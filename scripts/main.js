3610659ccff7c397076d2d4f14c6138cce7ae405
“Vladyslav.Fedoruk” <mr.fedoruk17@gmail.com>
2023-06-28 23:07:15 +0300

    validation

--- scripts/main.js
+++ scripts/main.js
@@ -1,43 +1,47 @@
 const year = +prompt('Вітаю! Підкажіть, будь ласка, ваш рік народження');
-    if (!year || isNaN(year) ) {
-        alert("Шкода, що Ви не захотіли ввести свій рік народження");
-    } else {
-        let currentYear = new Date().getFullYear();
-        let age = currentYear - year;
-        alert(`Ваш вік: ${age}`)
+if (!year || isNaN(year)) {
+    alert("Шкода, що Ви не захотіли ввести свій рік народження");
+} else {
+    let currentYear = new Date().getFullYear();
+    let age = currentYear - year;
+    alert(`Ваш вік: ${age}`)
 }
-    
 const city = prompt("В якому місті Ви живете?");
-    console.log(city);
-    if (!city) {
-        alert("Шкода, що Ви не захотіли ввести свіє місто в якому проживаєте");
-    } else {
-        switch (city) {
-            case "Київ": alert("Ти живеш у столиці України");
-                break
-            case "Лондон": alert("Ти живеш у столиці Англії");
-                break;
-            case "Вашингтон": alert("Ти живеш у столиці США");
-            default:
-                alert(`Ви проживаєте в місті ${city}`);
-                break;
-        }
+const hasSpecialCharacters = /[!@#$%^&*(),.?":{}|<>]/;
+if (!city || !isNaN(city) || hasSpecialCharacters.test(city)) {
+    alert("Шкода, що Ви не захотіли ввести свoє місто в якому проживаєте");
+} else {
+    switch (city) {
+        case "Київ":
+            alert("Ти живеш у столиці України");
+            break
+        case "Лондон":
+            alert("Ти живеш у столиці Англії");
+            break;
+        case "Вашингтон":
+            alert("Ти живеш у столиці США");
+            break
+        default:
+            alert(`Ви проживаєте в місті ${city}`);
+            break;
     }
-    const sport = prompt("Який Ваш улюблений вид спорту?");
-    if (!sport) {
-        alert("Шкода, що Ви не захотіли ввести свій улюблений вид спорту");
-        return;
-    } else {
-        switch (sport) {
-            case "Теніс": alert(`Ваш улюблений Вид спорту: ${sport}. Чемпіоном цього виду спорту вважається - Стефан Едберг`);
-                break
-            case "Футбол": alert(`Ваш улюблений Вид спорту: ${sport}. Чемпіоном цього виду спорту вважається - Ліонель Мессі`);
-                break;
-            case "Баскетбол": alert(`Ваш улюблений Вид спорту: ${sport}. Чемпіоном цього виду спорту вважається - Хосе Кальдерон`);
-                break
-            default:
-                alert(`Ваш улюблений вид спорту - ${sport}`);
-                break;
-        }
-    } 
-
+}
+const sport = prompt("Який Ваш улюблений вид спорту?");
+if (!sport || !isNaN(sport) || hasSpecialCharacters.test(sport)) {
+    alert("Шкода, що Ви не захотіли ввести свій улюблений вид спорту");
+} else {
+    switch (sport) {
+        case "Теніс":
+            alert(`Ваш улюблений Вид спорту: ${sport}. Чемпіоном цього виду спорту вважається - Стефан Едберг`);
+            break
+        case "Футбол":
+            alert(`Ваш улюблений Вид спорту: ${sport}. Чемпіоном цього виду спорту вважається - Ліонель Мессі`);
+            break;
+        case "Баскетбол":
+            alert(`Ваш улюблений Вид спорту: ${sport}. Чемпіоном цього виду спорту вважається - Хосе Кальдерон`);
+            break
+        default:
+            alert(`Ваш улюблений вид спорту - ${sport}`);
+            break;
+    }
+}
